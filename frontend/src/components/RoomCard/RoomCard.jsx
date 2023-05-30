91d27e081cb1a1766518e27a39b492202db9e93b
Alok3333 <alokprajapati697@gmail.com>
2023-05-30 22:44:06 +0530

    Now We created room's header. there is some heading and search-box and another site button of the start new room.

---
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RoomCard = () => {
+  return <div>This is room's card</div>;
+};
+
+export default RoomCard;
--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import styles from './Rooms.module.css';
 
 const Rooms = () => {
   return (
-    <div> Welcome to Rooms</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className={styles.roomHeader}>
+          <div className={styles.left}>
+            <span className={styles.heading}>All voice rooms</span>
+            <div className={styles.searchBox}>
+              <img src="/images/search-icon.png" alt="search" />
+              <input type="text" className={styles.searchInput} />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button className={styles.startRoomButton}>
+              <img src="/images/add-room-icon.png" alt="add-room" />
+              <span>Start a room</span>
+            </button>
+          </div>
+        </div>
 
-export default Rooms
+        <div className={styles.roomList}></div>
+      </div>
+    </>
+  );
+};
+
+export default Rooms;
