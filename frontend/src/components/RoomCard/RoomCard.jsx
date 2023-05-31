ec9183c05afb66c3cbaf9b682cd11f569872bdc1
Alok3333 <alokprajapati697@gmail.com>
2023-05-31 22:40:55 +0530

    Now i done room list show the card in frontend

--- frontend/src/components/RoomCard/RoomCard.jsx
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -20,6 +20,10 @@ const RoomCard = ({ room }) => {
           ))}
         </div>
       </div>
+      <div className={styles.peopleCount}>
+        <span>{room.totalPeople}</span>
+        <img src="/images/user-icon.png" alt="user-icon" />
+      </div>
     </div>
   );
 };
