7504a567d20a2aaeebb5293cfaceae2112cf0c93
Alok3333 <alokprajapati697@gmail.com>
2023-06-09 23:10:41 +0530

    Now we have get the data for current users and show with mic and only get one user from the voice meet room which can come inside the first those person are available.

--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,16 +1,56 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+import { useStateWithCallback } from './useStateWithCallback';
 
-export const useWebRTC = () => {
-  const [clients, setClients] = useState([
-    {
-      id: 1,
-      name: 'Rakesh k',
-    },
-    {
-      id: 2,
-      name: 'Jhon Doe',
-    },
-  ]);
+// const users = [
+//   {
+//     id: 1,
+//     name: 'Rakesh k',
+//   },
+//   {
+//     id: 2,
+//     name: 'Jhon Doe',
+//   },
+// ];
 
-  return { clients };
+export const useWebRTC = (roomId, user) => {
+  const [clients, setClients] = useStateWithCallback([]);
+  const audioElements = useRef({});
+  const connections = useRef({});
+  const localMediaStream = useRef(null);
+
+  const addNewClients = useCallback(
+    (newClient, cb) => {
+      const lookingFor = clients.find((client) => client.id === newClient.id);
+
+      if (lookingFor === undefined) {
+        setClients((existingClients) => [...existingClients, newClient], cb);
+      }
+    },
+    [clients, setClients]
+  );
+
+  // Capture media with computer
+  useEffect(() => {
+    const startCapture = async () => {
+      localMediaStream.current = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+    };
+
+    startCapture().then(() => {
+      addNewClients(user, () => {
+        const localElement = audioElements.current[user.id];
+        if (localElement) {
+          localElement.volume = 0;
+          localElement.scrObject = localMediaStream.current;
+        }
+      });
+    });
+  }, []);
+
+  const provideRef = (instance, userId) => {
+    audioElements.current[userId] = instance;
+  };
+
+  return { clients, provideRef };
 };
